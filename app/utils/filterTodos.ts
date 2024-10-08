9024aa97e86f914ebcecd1df2b01f9f01346641d
bhataasim1 <bhataasim700@gmail.com>
2024-10-08 19:22:24 +0530

    feat: Add filterTodos and searchTodo utility functions

---
+++ app/utils/filterTodos.ts
@@ -0,0 +1,24 @@
+import { FilterTodos, Todos } from "../types/types";
+
+const keys: (keyof Pick<Todos, 'title' | 'description'>)[] = ['title', 'description'];
+
+export function filterTodos(todos: Todos[], filter: FilterTodos) {
+  switch (filter) {
+    case "all":
+      return todos;
+    case "completed":
+      return todos.filter((todo) => todo.status === "completed");
+    case "pending":
+      return todos.filter((todo) => todo.status === "pending");
+    case "dueDate":
+      return todos.sort((a, b) => new Date(a.dueDate).getTime() - new Date(b.dueDate).getTime());
+    default:
+      return [];
+  }
+}
+
+export function searchTodo(todo: Todos[], query: string): Todos[] {
+  return todo.filter((todo) => {
+    return keys.some((key) => todo[key].toLowerCase().includes(query.toLowerCase()));
+  });
+}
