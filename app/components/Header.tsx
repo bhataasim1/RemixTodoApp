3af784f25e96889d33996010c4b3b636d89dd7e9
bhataasim1 <bhataasim700@gmail.com>
2024-10-10 18:51:31 +0530

    Refactor Header component to include logout functionality

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,19 @@
-import { Link } from "@remix-run/react"
+import { Link, useFetcher } from "@remix-run/react"
 
-export const Header = () => {
+export const Header = ({ userId }: { userId?: string | null }) => {
+  const fetcher = useFetcher();
   return (
     <div className="flex items-center justify-between border-b bg-purple-400 shadow-sm px-10 py-3">
       <h1 className="text-2xl font-bold text-white">
         <Link to="/">Remix Todo App</Link>
       </h1>
+      <div>
+        {userId && (
+          <fetcher.Form method="post" action="/logout">
+            <button type="submit" className="text-white font-medium">Logout</button>
+          </fetcher.Form>
+        )}
+      </div>
     </div>
   )
 }
