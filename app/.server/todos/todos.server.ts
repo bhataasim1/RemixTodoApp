a71d63b35dd054b37bb01b11f65dbb26c51ee50d
bhataasim1 <bhataasim700@gmail.com>
2024-10-11 14:22:18 +0530

    Refactor TodosService to extend BaseService and update CRUD methods

--- app/.server/todos/todos.server.ts
+++ app/.server/todos/todos.server.ts
@@ -1,94 +1,33 @@
 
-import { Todos } from "../../types/types";
-import directusClient from "../directus.server"
-import { createItem, deleteItem, readItem, readItems, updateItem } from "@directus/sdk";
+import { AddTodo, Todos } from "../../types/types";
+import { BaseService } from "./BaseService.server";
 
-type AddTodo = Pick<Todos, "title" | "description" | "dueDate" | 'userId'>;
-
-export class TodosService {
-  private client: typeof directusClient;
-
+export class TodosService extends BaseService {
   constructor() {
-    this.client = directusClient;
+    super();
     this.addTodo = this.addTodo.bind(this);
     this.editTodo = this.editTodo.bind(this);
     this.deleteTodo = this.deleteTodo.bind(this);
     this.getTodosOfUser = this.getTodosOfUser.bind(this);
   }
 
-  async addTodo(todo: AddTodo) {
-    // console.log(todo);
-    try {
-      const response = await this.client.request(
-        createItem("Todos", {
-          ...todo,
-          status: "pending",
-          createdAt: new Date()
-        })
-      )
-      // console.log(response);
-
-      return response;
-    } catch (error) {
-      // console.error(error);
-      throw new Error("Failed to add todo");
-    }
+  async addTodo(todo: AddTodo): Promise<Todos> {
+    return await this.createItem("Todos", todo);
   }
 
   async getUserTodo(userId: string, todoId: string): Promise<Todos> {
-    try {
-      return await this.client.request(
-        readItem("Todos", todoId, {
-          filter: {
-            userId: {
-              _eq: userId
-            }
-          }
-        })
-      );
-    } catch (error) {
-      console.error(error);
-      throw new Error("Failed to get todo");
-    }
+    return await this.readItem("Todos", todoId, userId);
   }
 
   async getTodosOfUser(userId: string): Promise<Todos[]> {
-    try {
-      return await this.client.request(
-        readItems("Todos", {
-          filter: {
-            userId: {
-              _eq: userId
-            }
-          }
-        })
-      );
-    } catch (error) {
-      console.error(error);
-      throw new Error("Failed to get todos");
-    }
+    return await this.readAllItems("Todos", userId);
   }
 
-
   async editTodo(todoId: string, todo: Partial<Todos>) {
-    try {
-      return await this.client.request(
-        updateItem("Todos", todoId, todo)
-      );
-    } catch (error) {
-      console.error(error);
-      throw new Error("Failed to edit todo");
-    }
+    return await this.updateTodoItem("Todos", todoId, todo);
   }
 
   async deleteTodo(todoId: string) {
-    try {
-      return await this.client.request(
-        deleteItem("Todos", todoId)
-      );
-    } catch (error) {
-      console.error(error);
-      throw new Error("Failed to delete todo");
-    }
+    return await this.deleteTodoItem("Todos", todoId);
   }
 }
